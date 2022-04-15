c45aad8b2dfdb9f9e43453f415dc89a0ddb82ae2
penspencap <871324352@qq.com>
2022-04-15 16:31:33 +0800

    feat #solana-etl 增加本地打包脚本

--- src/load/Upload.py
+++ src/load/Upload.py
@@ -1,8 +1,11 @@
 import os
+
 from google.cloud import storage
 from joblib import Parallel, delayed
+
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/solana_data/solana-etl/blockchain-data-process@footprint-blockchain-etl.iam.gserviceaccount.com.json'
 from argparse import ArgumentParser
+import os, tarfile
 
 
 def run_upload(data):
@@ -23,15 +26,26 @@ def upload_data_to_gcs(task, blocks, bucket='crypto_etl', n_jobs=4):
     _objects = f'solana_export/{mapper[task]}/{blocks}/'
     filename = f'/solana_data/bq_data/{task}/{blocks}/'
 
-    Parallel(n_jobs=n_jobs, backend='multiprocessing')(delayed(run_upload)((bucket, filename+_filename, _objects+_filename, )) for _filename in os.listdir(filename))
-
-
-def upload_block_raw_to_gcs(blocks, bucket='crypto_etl', n_jobs=4):
-    _objects = f'solana_export/blocks_raw/{blocks}/'
-    filename = f'/solana_data/data/{blocks}/'
     Parallel(n_jobs=n_jobs, backend='multiprocessing')(
-        delayed(run_upload)((bucket, filename + _filename, _objects + _filename,)) for _filename in os.listdir(filename)
-    )
+        delayed(run_upload)((bucket, filename + _filename, _objects + _filename,)) for _filename in
+        os.listdir(filename))
+
+
+def upload_block_raw_to_gcs(blocks, bucket='crypto_etl'):
+    _objects = f'solana_export/blocks_raw/{blocks}/'
+    _filename = f'{blocks}.gz'
+    filename = f'/solana_data/data/{blocks}/'
+    raw_block_dir = f'/solana_data/blocks_raw/{blocks}/'
+    _make_targz_one_by_one(raw_block_dir + _filename, filename)
+    run_upload((bucket, raw_block_dir + _filename, _objects + _filename))
+
+
+def _make_targz_one_by_one(output_filename, source_dir):
+    with tarfile.open(output_filename, "w:gz") as tar:
+        for root, _dir, files in os.walk(source_dir):
+            for file in files:
+                path_file = os.path.join(root, file)
+                tar.add(path_file)
 
 
 def main():
@@ -47,7 +61,7 @@ def main():
     for _data_type in ['blocks', 'transfers', 'transactions']:
         upload_data_to_gcs(_data_type, args.dir_blocks)
 
-    upload_block_raw_to_gcs(args.dir_blocks, n_jobs=args.n_jobs)
+    upload_block_raw_to_gcs(args.dir_blocks)
 
 
 if __name__ == '__main__':
